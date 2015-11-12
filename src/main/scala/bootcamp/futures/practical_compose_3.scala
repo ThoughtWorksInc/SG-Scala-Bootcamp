b26208a1a238371ee43cf669d4f4a8b550400206
Ravi Kumar Pasumarthy <ravikup@thoughtworks.com>
2015-11-12 18:48:11 +0800

    add second example for futures

--- src/main/scala/bootcamp/futures/practical_compose_3.scala
+++ src/main/scala/bootcamp/futures/practical_compose_3.scala
@@ -1,9 +1,6 @@
 package bootcamp.futures
 
-import java.io.File
-
 import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
  * Created by ravikupin on 11/11/15.
@@ -18,23 +15,33 @@ object practical_compose_3 extends App{
    */
   type WordCount = (String, Int)
 
+  /**
+   * Calculates the word count for a given file
+   * @param filename
+   * @return
+   */
+  def word_count_for_file(filename: String): Future[WordCount] = ???
+
   /** returns all the file names in a given directory
    *
    * @param directory
    * @return
    */
-  def files_in_directory(directory: String):Future[Seq[String]] = Future {
-    new File(directory).list().toSeq
-  }
+  def files_in_directory(directory: String):Future[Seq[String]] = ???
 
-  /** this method returns one future for each file in files
-   *
+  /** this method returns one future that contains the number of words for each file in files
+   * make use of word_count_for_file
    * @param files
    * @return
    */
-  def total_word_count_for_files_internal(files: Seq[String]):Seq[Future[WordCount]] = files map word_count_for_file
+  def total_word_count_for_files_internal(files: Seq[String]):Seq[Future[WordCount]] = ???
 
+  /**
+   * Given the name the directory, this method calculates total wordcounts for all files
+   * make use of methods total_word_count_for_files_internal and files_in_directory
+   * @param directory
+   * @return
+   */
   def total_word_count_for_each_file_in_directory(directory: String): Future[Seq[WordCount]] = ???
 
-  def word_count_for_file(filename: String): Future[WordCount] = ???
 }
